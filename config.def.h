5ffe467392975df026c1b77ee8929cdd9e9d125f
Nikita K <obama.library@nara.gov>
2024-09-17 13:32:33 +0300

    patch cursorwrap
    add patch cursor wrap & patch
    and change firefox to tile from float layout

--- config.def.h
+++ config.def.h
@@ -36,7 +36,7 @@ static const Rule rules[] = {
 	 */
 	/* class              instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",             NULL,       NULL,       0,            1,           -1 },
-	{ "firefox",          "Navigator", NULL,       1 << 0,       1,          -1 },
+	{ "firefox",          "Navigator", NULL,       1 << 0,       0,          -1 },
 	{ "Google-chrome",    NULL,        NULL,       1 << 1,       0,          -1 },
 	{ "TelegramDesktop",  NULL,        NULL,       1 << 2,       1,          -1 },
 };
